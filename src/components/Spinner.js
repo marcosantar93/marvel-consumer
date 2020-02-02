f0c32688ffdf535ae2d76952a743a41dae35e447
Marco Santarcangelo Zazzetta <marcosantar93@gmail.com>
2020-02-02 20:58:51 -0300

    loading spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const Spinner = props => {
   return (
-    <div className="ui active dimmer">
+    <div className="ui active inverted dimmer">
       <div className="ui big text loader">{props.message}</div>
     </div>
   );
 };
 
 Spinner.defaultProps = {
-  message: "Loading ..."
+  message: "Loading..."
 };
 
 export default Spinner;

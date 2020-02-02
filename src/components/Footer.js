50b60d913592b2bbc62496f5a9bbb22fa0f34731
Marco Santarcangelo Zazzetta <marcosantar93@gmail.com>
2020-02-02 20:58:44 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Footer = () => {
+const Footer = props => {
   return (
-    <div className="ui secondary pointing menu">
+    <div className={`ui secondary pointing menu ${props.className}`}>
       <a href="http://marvel.com">Data provided by Marvel. © 2020 MARVEL</a>
     </div>
   );
